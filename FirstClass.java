80188c7665f76b5d67f460ced2d1383c2c716445
Divyanshu Verma <divyanshuverma2203@gmail.com>
2026-07-20 22:34:15 +0530

    add new patterns

--- FirstClass.java
+++ FirstClass.java
@@ -1,3 +1,4 @@
+package study;
 class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
--- Pattern.java
+++ Pattern.java
@@ -516,27 +516,112 @@ package study;
 
 
 
+// public class Pattern {
+//     public static void main(String[] args) {
+//         int n=5;
+
+//         for (int i=1;i<=n ; i++) {
+//             for (int j=1; j<=i; j++) {
+//                 int sum = i+j;
+//                 if (sum %2==0) {
+//                     System.out.print("1");
+//                 } else {
+//                     System.out.print("0");
+//                 }
+            
+//             }
+//             System.out.println();
+//         }
+//     }
+
+// }
+
+
+
+
+
+
+
+
+// QUES 10.         BUTTERFLY PATTERN
+
+
+// public class Pattern {
+//     public static void main (String[] args) {
+//         int n =5;
+
+//         // upper half
+//         for (int i=1; i<=n; i++) {
+//             // 1st part
+//             for (int j=1; j<=i; j++) {
+//                 System.out.print("*");
+//             }
+
+//             // spaces
+//             int spaces = 2 * (n-i);
+//             for (int j=1; j<=spaces; j++) {
+//                 System.out.print(" ");
+//             }
+
+//             // 2nd part
+
+//             for(int j=1; j<=i; j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//         // lower half
+
+//         for (int i=n; i>=1; i--) {
+//             // 1st part
+//             for (int j=1; j<=i; j++) {
+//                 System.out.print("*");
+//             }
+
+//             // spaces
+//             int spaces = 2 * (n-i);
+//             for (int j=1; j<=spaces; j++) {
+//                 System.out.print(" ");
+//             }
+
+//             // 2nd part
+
+//             for(int j=1; j<=i; j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+
+//     }
+// }
+
+
+
+
+
+
+//    SOLID RHOMBUS PATTERN
+
+
+
+
+
+
 public class Pattern {
     public static void main(String[] args) {
         int n=5;
 
-        for (int i=1;i<=n ; i++) {
-            for (int j=1; j<=i; j++) {
-                int sum = i+j;
-                if (sum %2==0) {
-                    System.out.print("1");
-                } else {
-                    System.out.print("0");
-                }
-            
+        for (int i=1; i<=n; i++) {
+            // spaces
+            for (int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for (int j=1; j<=n; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
     }
-
 }
-
-
-
-
-
