85ac91aff95e48d864ab7c62914a0b7db0d433d2
Divyanshu Verma <divyanshuverma2203@gmail.com>
2026-07-20 23:43:07 +0530

    add new code

--- Pattern.java
+++ Pattern.java
@@ -607,6 +607,105 @@ package study;
 
 
 
+// public class Pattern {
+//     public static void main(String[] args) {
+//         int n=5;
+
+//         for (int i=1; i<=n; i++) {
+//             // spaces
+//             for (int j=1; j<=n-i; j++) {
+//                 System.out.print(" ");
+//             }
+
+//             // stars
+//             for (int j=1; j<=n; j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+//  HOLLOW RHOMBUS PATTERN
+
+
+
+
+// public class Pattern {
+//     public static void main(String[] args) {
+//         int n=5;
+
+//         for (int i=1; i<=n; i++) {
+//             // spaces
+//             for (int j=1; j<=n-i; j++) {
+//                 System.out.print(" ");
+//             }
+
+//             // stars
+//             for (int j=1; j<=n; j++) {
+//                 if (i==1 || i==n || j==1 || j==n) {
+//                     System.out.print("*");
+//                 } else {
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+// number pyramid pattern
+
+
+
+
+
+// public class Pattern {
+//     public static void main(String[] args) {
+//         int n=5;
+
+//         for (int i=1; i<=n; i++) {
+//             // spaces
+//             for (int j=1; j<=n-i; j++) {
+//                 System.out.print(" ");
+//             }
+
+//             // numbers
+//             for (int j=1; j<=i; j++) {
+//                 System.out.print(i+" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+// PATTERN 14.        PALINDROME PATTERN
+
+
+
+
 public class Pattern {
     public static void main(String[] args) {
         int n=5;
@@ -617,11 +716,16 @@ public class Pattern {
                 System.out.print(" ");
             }
 
-            // stars
-            for (int j=1; j<=n; j++) {
-                System.out.print("*");
+            // numbers
+            for (int j=i; j>=1; j--) {
+                System.out.print(j);
+            }
+
+            for (int j=2; j<=i; j++) {
+                System.out.print(j);
             }
             System.out.println();
         }
     }
 }
+
