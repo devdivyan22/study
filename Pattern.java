2c1f5badd8a35872a79f5d210b53c86e9978885e
Divyanshu Verma <divyanshuverma2203@gmail.com>
2026-07-21 08:00:42 +0530

    add new code

--- Pattern.java
+++ Pattern.java
@@ -706,26 +706,71 @@ package study;
 
 
 
+// public class Pattern {
+//     public static void main(String[] args) {
+//         int n=5;
+
+//         for (int i=1; i<=n; i++) {
+//             // spaces
+//             for (int j=1; j<=n-i; j++) {
+//                 System.out.print(" ");
+//             }
+
+//             // numbers
+//             for (int j=i; j>=1; j--) {
+//                 System.out.print(j);
+//             }
+
+//             for (int j=2; j<=i; j++) {
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
+
+
+// PATTERN 15.        DIAMOND PATTERN
+
+
+
+
+
+
 public class Pattern {
     public static void main(String[] args) {
         int n=5;
 
+        // upper half
         for (int i=1; i<=n; i++) {
             // spaces
             for (int j=1; j<=n-i; j++) {
                 System.out.print(" ");
             }
 
-            // numbers
-            for (int j=i; j>=1; j--) {
-                System.out.print(j);
+            // stars
+            for (int j=1; j<=(2*i)-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // lower half
+        for (int i=n; i>=1; i--) {
+            // spaces
+            for (int j=1; j<=n-i; j++) {
+                System.out.print(" ");
             }
 
-            for (int j=2; j<=i; j++) {
-                System.out.print(j);
+            // stars
+            for (int j=1; j<=(2*i)-1; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
     }
 }
-
